2bbfef835fccbc2a917b0fedb03076f4ee2651a6
Billie-K <billiemush@gmail.com>
2021-09-13 10:27:39 -0700

    fix print list integer

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
+    """function that prints integers in a list."""
     for j in range(len(my_list)):
         print('{:d}'.format(my_list[j]))
